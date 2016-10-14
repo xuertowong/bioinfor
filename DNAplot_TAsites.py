3b43e29696e79c5e7322ad60f303766071fa3237
xuertowong <xuertowong@gmail.com>
2016-10-14 20:13:07 +0800

    DNAplot TAsites prepare

---
+++ DNAplot_TAsites.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+
+def TAfinder(genome, tacsv, address):
+    gene = ''
+    for line in open(genome):
+        gene = gene+line.strip()
+    ta = pd.read_csv(tacsv)
+    site = np.zeros((len(gene),1))
+    for i in range(len(ta)):
+        if ta['reads'][i] != 0
+            site[ta['sites']] = ta['reads'][i]
+    output = open(address,'w')
+    for i in range(len(site)):
+        output.write(str(int(site[i]))+'\n')
+        
+        
