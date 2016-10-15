9fc00d9a7de145c99a6a2b3de63344d7c9984281
xuertowong <xuertowong@gmail.com>
2016-10-15 15:10:44 +0800

    debug

--- DNAplot_TAsites.py
+++ DNAplot_TAsites.py
@@ -9,10 +9,11 @@ def TAfinder(genome, tacsv, address):
     ta = pd.read_csv(tacsv)
     site = np.zeros((len(gene),1))
     for i in range(len(ta)):
-        if ta['reads'][i] != 0
-            site[ta['sites']] = ta['reads'][i]
+        if ta['reads'][i] != 0:
+            site[ta['sites'][i]] = ta['reads'][i]
     output = open(address,'w')
     for i in range(len(site)):
         output.write(str(int(site[i]))+'\n')
+    output.close()
         
         
