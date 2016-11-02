7f23fa4c88bbee17924e7a26afb3c357d3027686
xuertowong <xuertowong@gmail.com>
2016-11-02 23:04:05 +0800

    gene

---
+++ cutGeneFIMO2.py
@@ -0,0 +1,31 @@
+import numpy as np
+    
+def cutGene(filename,genefile,file):
+    gene = ''
+    for line in open(genefile):
+        if line.find('>') ==-1:
+            gene = gene+line.strip()
+        else:
+            continue
+    myfile = open(file,'w')
+    start = np.zeros((len(filename),1))
+    end = np.zeros((len(filename),1))
+    for i in range(len(filename)):
+        if filename[i][0] != '':
+            start[i] = filename[i][1]-300
+            end[i] = filename[i][2]
+        else: continue
+    length = len(start)
+    for i in range(length):
+        if start[i] != 0:
+            a = start[i]; b = end[i]
+            c = gene[int(a)-1:int(b)]
+            myfile.write('>'+str(filename[i][0])+'\n')
+            myfile.write(str(c)+'\n')    
+        else:
+            continue
+    myfile.close()
+    return 0
+    
+    
+    
---
+++ genomeReversecomplement.py
@@ -0,0 +1,11 @@
+from Bio.Seq import Seq
+
+def genomeReverse(fasta,savefile):
+    genome = ''
+    for line in open(fasta):
+        genome = genome+line.strip()
+    genome = Seq(genome)
+    genome = genome.reverse_complement()
+    txt = open(savefile,'w')
+    txt.write(genome)
+    txt.close()
