ab4b67a8fd438cfe26f3e47b0a50524ec54b54d4
xuertowong <xuertowong@gmail.com>
2016-11-14 16:30:58 +0800

    1

--- cutGeneFIMO2.py
+++ cutGeneFIMO2.py
@@ -1,31 +1,39 @@
 import numpy as np
-    
-def cutGene(filename,genefile,file):
-    gene = ''
-    for line in open(genefile):
-        if line.find('>') ==-1:
-            gene = gene+line.strip()
-        else:
+import sys
+from matplotlib.mlab import csv2rec
+
+filename = sys.argv[1]
+genefile = sys.argv[2]
+num = sys.argv[3]
+csv = csv2rec(filename)
+gene = ''
+for line in open(genefile):
+    if line.find('>') ==-1:
+        gene = gene+line.strip()
+    else:
+        continue
+myfile = open(filename.split('.')[0]+'.txt','w')
+start = np.zeros((len(csv),1))
+end = np.zeros((len(csv),1))
+for i in range(len(csv)):
+    if '-' in filename:
+        start[i] = csv[i][2]
+        end[i] = csv[i][2] + int(num)
+    if '+' in filename:
+        start[i] = csv[i][1] - int(num)
+        end[i] = csv[i][1]
+    else: continue
+length = len(start)
+for i in range(length):
+    if start[i] != 0:
+        a = start[i]; b = end[i]
+        c = gene[int(a)-1:int(b)]
+        myfile.write('>'+str(csv[i][0])+'\n')
+        myfile.write(str(c)+'\n')    
+    else:
             continue
-    myfile = open(file,'w')
-    start = np.zeros((len(filename),1))
-    end = np.zeros((len(filename),1))
-    for i in range(len(filename)):
-        if filename[i][0] != '':
-            start[i] = filename[i][1] - 300
-            end[i] = filename[i][1]
-        else: continue
-    length = len(start)
-    for i in range(length):
-        if start[i] != 0:
-            a = start[i]; b = end[i]
-            c = gene[int(a)-1:int(b)]
-            myfile.write('>'+str(filename[i][0])+'\n')
-            myfile.write(str(c)+'\n')    
-        else:
-            continue
-    myfile.close()
-    return 0
+myfile.close()
+
     
     
     
--- genomeReversecomplement.py
+++ genomeReversecomplement.py
@@ -1,15 +1,15 @@
 from Bio.Seq import Seq
-
-def genomeReverse(fasta): 
-    savefileName = fasta.split('.')[0]
-    txt = open(savefileName+'-.fasta','w')
-    for line in open(fasta):
-        if line.startswith('>'):
-            txt.write(line+'\n')
-            genome = ''
-        else:
-            genome = genome+line.strip()
-    genome = Seq(genome)
-    genome = genome.reverse_complement()    
-    txt.write(str(genome))
-    txt.close()
+import sys
+fasta = sys.argv[1] 
+txt = open(fasta.split('.')[0]+'-.fasta','w')
+genome = ''
+for line in open(fasta):
+    if line.startswith('>'):
+        txt.write(line)
+        
+    else:
+        genome = genome+line.strip()
+genome = Seq(genome)
+genome = genome.reverse_complement()    
+txt.write(str(genome))
+txt.close()
