41e6174a8486e8416a37551fcabe04f8e9e5c750
xuertowong <xuertowong@gmail.com>
2017-01-05 10:17:45 +0800

    co

--- countGeneInsert.py
+++ countGeneInsert.py
@@ -1,25 +1,36 @@
 import pandas as pd
 import numpy as np
-tafile = pd.read_csv("C:\\Users\\lenovo\\425ta.csv")
-chrfile = pd.read_csv("C:\\Users\\lenovo\\425chr2.csv")
+import os
+import sys
+
+address1 = sys.argv[1]
+address2 = sys.argv[2]
+fileName = address1.split('.')[0] + 'output.csv'
+tafile = pd.read_csv(address1)
+chrfile = pd.read_csv(address2)
 
 tasite = list(tafile['tasite'])
 tanums = list(tafile['num'])
 start = list(chrfile['start'])
 end = list(chrfile['end'])
+
+def length(x, y):
+    length = abs(y - x) * 0.1
+    x = x + length
+    y = y - length
+    return x,y
+    
 c = np.zeros((np.shape(start)))
-for j in range(len(start)):
-    length = end[j] - start[j]
-    length = 0.1*length
-    start[j] = start[j] + length
-    end[j] = end[j] - length
+startend= list(map(length, start, end))
+startend = list(map(list, startend))
+
 for i in range(len(tanums)):
     if tanums[i] > 0:
         for j in range(len(start)):
-            if tasite[i] > start[j] and tasite[i] < end[j]:
+            if tasite[i] > startend[j][0] and tasite[i] < startend[j][1]:
                 c[j] = c[j] + tanums[i]
     else:
         continue
                 
 e = pd.DataFrame(c)
-e.to_csv("C:\\Users\\lenovo\\15.csv")
+e.to_csv(fileName)
